7ab437c84053f895f0df34aaa0c9daf772c2ea92
Amr Wagdy <amrw.js@gmail.com>
2025-11-03 15:18:13 +0200

    fix: env vars

--- app/helpers/getApiUrl.ts
+++ app/helpers/getApiUrl.ts
@@ -6,9 +6,15 @@ export const getApiUrl = () => {
       return process.env.NEXT_PUBLIC_API_DEV_BASE_URL;
 
     case "preview":
-      return process.env.NEXT_PUBLIC_API_DEV_BASE_URL;
+      return process.env.NEXT_PUBLIC_API_PREVIEW_BASE_URL;
+
+    case "production":
+      return (
+        process.env.NEXT_PUBLIC_API_PROD_BASE_URL ||
+        process.env.NEXT_PUBLIC_API_PREVIEW_BASE_URL
+      );
 
     default:
-      return process.env.NEXT_PUBLIC_API_BASE_URL;
+      return process.env.NEXT_PUBLIC_API_DEV_BASE_URL;
   }
 };
