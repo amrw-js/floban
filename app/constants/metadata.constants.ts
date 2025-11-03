d7a665c24d3e213fdf5f58cb7e7e2691f5edd489
Amr Wagdy <amrw.js@gmail.com>
2025-11-03 15:32:46 +0200

    chore: Add metadata to the app

---
+++ app/constants/metadata.constants.ts
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "🗂️ FloBan | Simple Kanban Board App",
+  description:
+    "FloBan is a lightweight Kanban board built with Next.js, Redux Toolkit, and Tailwind CSS — manage your tasks easily with drag-and-drop and mock backend support.",
+  keywords: [
+    "Kanban",
+    "Task Manager",
+    "Next.js",
+    "React",
+    "Redux Toolkit",
+    "Tailwind CSS",
+    "json-server",
+    "FloBan",
+    "Productivity App",
+  ],
+  authors: [{ name: "Amr Wagdy" }],
+  creator: "Amr Wagdy",
+  publisher: "FloBan",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
